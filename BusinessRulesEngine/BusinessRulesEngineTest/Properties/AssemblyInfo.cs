21a27e02842189072791078ab9130430c132b0f4
Varsha Chakravorty <varsha.chakravorty@siemens.com>
2020-07-28 08:37:36 +0530

    Added first failing test

---
+++ BusinessRulesEngine/BusinessRulesEngineTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BusinessRulesEngineTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BusinessRulesEngineTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("778cdef3-1147-4118-be65-798616542538")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BusinessRulesEngine/BusinessRulesEngineTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BusinessRulesEngineTest
+{
+    [TestFixture]
+    public class PaymentTest
+    {
+        [Test]
+        public void DoPostPaymentTest_ForPhysicalProduct_GeneratesAPackagingSlip()
+        {
+            //Arrange
+            bool expectedResult = true;
+             PhysicalProductsPayment physicalProductsPayment= new PhysicalProductsPayment();
+            //Act
+            bool isPackagingSlipGenerated=physicalProductsPayment.DoPostPayment();
+            //Assert
+            Assert.That(isPackagingSlipGenerated,Is.EqualTo(expectedResult));
+        }
+    }
+}
